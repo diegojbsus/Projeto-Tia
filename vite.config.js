5428ad55fcd4a26194440b08fb4b7a4565b181ea
diegojbsus <diegojbs@gmail.com>
2025-11-17 23:15:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  
   build: {
     outDir: 'dist',
     emptyOutDir: true
-  },
-
-  // Not used in production, only local dev
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5174',
-      '/images': 'http://localhost:5174'
-    }
   }
 })
